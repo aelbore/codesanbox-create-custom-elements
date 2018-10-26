ecaefb8615f1434328ce2e5a18c6c84585cb67d0
aelbore <aelbore.sg@gmail.com>
2018-10-26 21:14:50 +0800

    remove css and html file for app

--- src/app/app.element.ts
+++ src/app/app.element.ts
@@ -2,8 +2,9 @@ import { CustomElement } from 'custom-elements-ts';
 
 @CustomElement({
   tag: 'app-root',
-  templateUrl: './app.element.html',
-  styleUrl: './app.element.scss',
+  templateUrl: `
+    <h1>Hello World</h1>
+  `,
   shadow: false
 })
 export class AppElement extends HTMLElement { }
